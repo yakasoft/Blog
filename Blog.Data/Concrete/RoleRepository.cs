b25d846aa1409d76a5c0f20a960e389bfa3e1562
yunus.yaka <yakay@DESKTOP-EO2GO0C>
2022-08-17 00:03:41 +0300

    Add project files.

---
+++ Blog.Data/Concrete/RoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Data.Abstract;
+using Blog.Entities.Concrete;
+using Blog.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Concrete
+{
+    public class RoleRepository : EfEntityRepositoryBase<Role>, IRoleRepository
+    {
+        public RoleRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
